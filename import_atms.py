8f23d7bc31f74ba112115927736fbb8a4eae998f
Matias A. Rodriguez Cheroky <fisu@yahoo.com>
2024-05-02 23:09:42 -0300

    agregado import_atms

---
+++ import_atms.py
@@ -0,0 +1,13 @@
+def connectDb():
+    from sqlalchemy import create_engine
+    import configparser
+
+    print("Conectando DB ...")
+    motor = create_engine('mysql+mysqlconnector://matiasrodr:fisuMARswi966@localhost/tdf_automatic')
+    return motor
+
+import pandas as pd
+print("Importando datos de ATMs a la DB ...")
+datos_excel = pd.read_excel("ATMsOperativosMacro.xlsx")
+retorno = datos_excel.to_sql(name="atms", con=connectDb(), if_exists="replace", index=False, index_label="ATM")
+print(retorno)
